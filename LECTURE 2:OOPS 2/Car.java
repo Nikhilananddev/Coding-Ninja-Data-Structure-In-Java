2e0a866ea1d1b248f2bcc02904dc0d9471e5694f
HITESH CHOUDHARY <hcjat70@gmail.com>
2020-03-26 17:53:56 +0530

    Add files via upload

---
+++ LECTURE 2:OOPS 2/Car.java
@@ -0,0 +1,5 @@
+
+public class Car extends Vehicle {
+	int numGears;
+	boolean isConvertible;
+}
---
+++ LECTURE 2:OOPS 2/VehicleUse.java
@@ -0,0 +1,13 @@
+
+public class VehicleUse {
+
+	public static void main(String[] args) {
+		Vehicle v = new Vehicle();
+		v.print();
+		
+		Car c = new Car();
+		c.numGears = 10;
+		c.color = "Black";
+		c.print();
+	}
+}
