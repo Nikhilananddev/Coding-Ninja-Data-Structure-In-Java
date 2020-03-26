f3c74d929cbaec9accf63707c1dcef436321ec7b
HITESH CHOUDHARY <hcjat70@gmail.com>
2020-03-26 17:53:18 +0530

    Create vehicle.java

---
+++ LECTURE 2:OOPS 2/vehicle.java
@@ -0,0 +1,12 @@
+
+public class Vehicle 
+{
+	String color;
+	int maxSpeed;
+	
+	public void print() {
+		System.out.println("Vehicle color : " + color);
+		System.out.println("Vehicle Speed : " + maxSpeed);
+	}
+	
+}
